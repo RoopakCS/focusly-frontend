b61b9fae4aa6ea12e308cc59b13663d59e1bf83d
RoopakCS <roopaksenthilkumar@gmail.com>
2025-07-23 17:47:50 +0530

    created apis for todos

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-import SERVER_URL from "./SERVER_URL"
+import SERVER_URL from "../SERVER_URL"
 
 const api = axios.create({
   baseURL: `https://${SERVER_URL}`,
---
+++ src/api/todoAPI.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const API = "https://localhost:3000/api/todo";
+
+export const getTodos = () => axios.get(API);
+export const createTodo = (todo) => axios.post(API, todo);
+export const UpdateTodo = (id, data) => axios.patch(`${API}/${id}`, data);
+export const deleteTodo = (id) => axios.delete(`${API}/${id}`);
+export const clearTodos = () => axios.delete(API);
