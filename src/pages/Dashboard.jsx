35e400b3f5770a951c14a05227924e489431f484
RoopakCS <roopaksenthilkumar@gmail.com>
2025-07-24 10:24:35 +0530

    created username input in dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,10 +1,36 @@
+import { useState } from "react";
 import PomodoroTimer from "../components/PomodoroTimer";
 import TodoList from "../components/TodoList";
 
 function Dashboard() {
+  const [user, setUser] = useState("Guest");
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    setIsVisible(!isVisible);
+  };
   return (
     <div className="p-4">
       <h1 className="text-2xl font-bold">Welcome to Focusly Dashboard</h1>
+      <div>
+        {isVisible ? (
+          <input
+            type="text"
+            placeholder="Enter user name"
+            onChange={(e) => {
+              setUser(e.target.value);
+              localStorage.setItem("username", e.target.value);
+            }}
+            value={user}
+          />
+        ) : (
+          <h1>{localStorage.getItem("username")}</h1>
+        )}
+        <button onClick={toggleVisibility}>
+          {isVisible ? "Update Username" : "✏️"}
+        </button>
+      </div>
+
       <TodoList />
       <PomodoroTimer />
     </div>
